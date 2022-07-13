0fd1265e9edc993a34c1b853144065ed222fb5df
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-07-13 16:53:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Tarun sai</h1>
+      <h1>Hello Tarun sai .k</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
